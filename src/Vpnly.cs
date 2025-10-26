dffa203b6b50f8ebbca74f0ace452313600371e7
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-26 03:58:31 +0300

    Update and rename vpnly.sh to Vpnly.cs

---
+++ src/Vpnly.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Net.Http;
+
+namespace VpnlyApi
+{
+    public class Vpnly
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://s3.hub-vpn.com";
+        public Vpnly()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+        }
+
+        public async Task<string> GetServers()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/servers.json");
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
